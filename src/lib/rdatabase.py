c9b818dc4464479c91d4420ad2fdb8595869ea63
thomas_hinkle <thomas_hinkle>
2005-02-01 04:12:42 +0000

    Moved a number of universal functions from rmetakit to here. Implemented Undo functionality for modify_rec (as a testcase).

--- src/lib/rdatabase.py
+++ src/lib/rdatabase.py
@@ -2,7 +2,8 @@ import os.path
 from gdebug import debug, TimeAction
 import re, pickle, keymanager, string, shopping, convert, os.path
 from gettext import gettext as _
-import gglobals
+import gglobals, Undo
+from defaults import lang as defaults
 
 
 class RecData:
@@ -103,11 +104,174 @@ class RecData:
             debug('running hook %s with args %s'%(h,args),3)
             h(*args)
 
+    def undoable_modify_rec (self, rec, dic, history=[], get_current_rec_method=None):
+        orig_dic = {}
+        for k in dic.keys():
+            orig_dic[k]=getattr(rec,k)
+        redo,reundo=None,None
+        if get_current_rec_method:
+            redo=lambda *args: [get_current_rec_method(),dic]
+            reundo = lambda *args: [get_current_rec_method(),orig_dic]
+        obj = Undo.UndoableObject(self.modify_rec,self.modify_rec,history,
+                                  action_args=[rec,dic],undo_action_args=[rec,orig_dic],
+                                  get_reapply_action_args=redo,
+                                  get_reundo_action_args=reundo)
+        obj.perform()
+
     def modify_rec (self, rec, dict):
         raise NotImplementedError
 
     def search (self, table, colname, text, exact=0, use_regexp=True):
         raise NotImplementedError
+
+    def get_default_values (self, colname):
+        try:
+            return defaults.fields[colname]
+        except:
+            return []
+
+    def get_unique_values (self, colname,table=None):
+        if not table: table=self.rview
+        dct = {}
+        if defaults.fields.has_key(colname):
+            for v in defaults.fields[colname]:
+                dct[v]=1
+        def add_to_dic (row):
+            a=getattr(row,colname)
+            if type(a)==type(""):
+                for i in a.split(","):
+                    dct[i.strip()]=1
+            else:
+                dct[a]=1
+        table.filter(add_to_dic)
+        return dct.keys()
+
+    def get_ings (self, rec):
+        raise NotImplementedError
+
+    def order_ings (self, iview):
+        """Handed a view of ingredients, we return an alist:
+        [['group'|None ['ingredient1', 'ingredient2', ...]], ... ]
+        """
+        defaultn = 0
+        groups = {}
+        group_order = {}
+        for i in iview:
+            # defaults
+            if not hasattr(i,'group'):
+                group=None
+            else:
+                group=i.inggroup
+            if not hasattr(i,'position'):
+                i.position=defaultn
+                defaultn += 1
+            if groups.has_key(group): 
+                groups[group].append(i)
+                # the position of the group is the smallest position of its members
+                # in other words, positions pay no attention to groups really.
+                if i.position < group_order[group]: group_order[group]=i.position
+            else:
+                groups[group]=[i]
+                group_order[group]=i.position
+        # now we just have to sort an i-listify
+        def sort_groups (x,y):
+            if group_order[x[0]] > group_order[y[0]]: return 1
+            elif group_order[x[0]] == group_order[y[0]]: return 0
+            else: return -1
+        alist=groups.items()
+        alist.sort(sort_groups)
+        def sort_ings (x,y):
+            if x.position > y.position: return 1
+            elif x.position == y.position: return 0
+            else: return -1
+        for g,lst in alist:
+            lst.sort(sort_ings)
+        debug('alist: %s'%alist,5)
+        return alist
+
+    def ingview_to_lst (self, view):
+        """Handed a view of ingredient data, we output a useful list.
+        The data we hand out consists of a list of tuples. Each tuple contains
+        amt, unit, key, alternative?"""
+        for i in view:
+            ret.append([i.amount, i.unit, i.ingkey,])
+        return ret
+    
+    def ing_shopper (self, view):
+        return mkShopper(self.ingview_to_lst(view))
+        
+    def get_rec (self, id, rview=None):
+        if not rview:
+            rview=self.rview
+        s = rview.select(id=id)
+        if len(s)>0:
+            return rview.select(id=id)[0]
+        else:
+            return None
+
+    def delete_rec (self, rec):
+        raise NotImplementedError
+
+    def new_rec (self):
+        raise NotImplementedError
+
+    def add_ing (self, ingdict):
+        """Add ingredient to iview based on ingdict and return
+        ingredient object. Ingdict contains:
+        id: recipe_id
+        unit: unit
+        item: description
+        key: keyed descriptor
+        alternative: not yet implemented (alternative)
+        optional: yes|no
+        position: INTEGER [position in list]
+        refid: id of reference recipe. If ref is provided, everything
+               else is irrelevant except for amount.
+        """
+        debug('add_ing ingdict=%s'%ingdict,5)
+        self.changed=True
+        debug('removing unicode',3)
+        timer = TimeAction('rmetakit.add_ing 1',0)
+        self.remove_unicode(ingdict)
+        timer.end()
+        debug('adding to iview %s'%ingdict,3)
+        timer = TimeAction('rmetakit.add_ing 2',0)
+        self.iview.append(ingdict)
+        timer.end()
+        debug('running ing hooks %s'%self.add_ing_hooks,3)
+        timer = TimeAction('rmetakit.add_ing 3',0)
+        self.run_hooks(self.add_ing_hooks, self.iview[-1])
+        timer.end()
+        debug('done with ing hooks',3)
+        return self.iview[-1]
+
+    def modify_ing (self, ing, ingdict):
+        #self.delete_ing(ing)
+        #return self.add_ing(ingdict)
+        for k,v in ingdict.items():
+            if hasattr(ing,k):
+                self.changed=True
+                setattr(ing,k,v)
+            else:
+                debug("Warning: ing has no attribute %s (attempted to set value to %s" %(k,v),0)
+        return ing
+
+    def replace_ings (self, ingdicts):
+        """Add a new ingredients and remove old ingredient list."""
+        ## we assume (hope!) all ingdicts are for the same ID
+        id=ingdicts[0]['id']
+        debug("Deleting ingredients for recipe with ID %s"%id,1)
+        ings = self.get_ings(id)
+        for i in ings:
+            debug("Deleting ingredient: %s"%i.ingredient,5)
+            self.delete_ing(i)
+        for ingd in ingdicts:
+            self.add_ing(ingd)
+
+    def delete_ing (self, ing):
+        raise NotImplementedError
+
+    
 
     
 class RecipeManager (RecData):
@@ -163,18 +327,14 @@ class RecipeManager (RecData):
             debug("Unable to parse %s"%string,0)
             return None
 
+    def ing_search (self, ing, keyed=None, rview=None, use_regexp=True, exact=False):
+        raise NotImplementedError
 
-        #if self.kd.has_key(ing):
-        #    #print "DEBUG: key_search returning:",self.kd[ing]
-        #    return self.kd[ing]
-        #else:
-        #    
-        #    retlist = []
-        #    for i in self.kd.keys():
-        #        if string.find(i,ing) > -1 or string.find(ing,i) > -1:
-        #            retlist.extend(self.kd[i])
-        #    #print "DEBUG: key_search returning:",retlist
-        #    return retlist
+    def ings_search (self, ings, keyed=None, rview=None, use_regexp=True, exact=False):
+        raise NotImplementedError
+
+    
+
 
 class mkConverter(convert.converter):
     def __init__ (self, db):
