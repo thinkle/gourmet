ee89fc6d6c67a2794177b1447b6b493886f4ea77
thomas_hinkle <thomas_hinkle>
2005-03-03 12:28:02 +0000

    Always ask to confirm command.

--- src/lib/exporters/lprprinter.py
+++ src/lib/exporters/lprprinter.py
@@ -6,16 +6,17 @@ from gettext import gettext as _
 class RecRenderer:
     def __init__ (self, rd, recs, mult=1, dialog_title=_("Print Recipes"),
                   dialog_parent=None, change_units=True):        
-        command = 'lpr'
+        command = self.get_command(label='Enter print command',
+                                   sublabel='Please enter the command you would like to use to print.',
+                                   default_value='lpr')
         # TO TEST LOUSY COMMAND-ENTERING CODE, UNCOMMENT THE FOLLOWING
         #command = 'asdf'
-        while os.system('which %s'%command) != 0:
-            label = _("Unable to find command %s.  ")%command
-            label += _('Enter a print command')
-            command = de.getEntry(label=_('Enter print command'),
-                                  sublabel=label,
-                                  entryLabel=_('Command'))
-            if not command: raise "User cancelled!"
+        while os.system('which %s'%command.split()[0]) != 0:
+            label = _("Enter print command")
+            sublabel = _("Unable to find command \"%s\".")%command
+            sublabel += _('Please enter the command you would like to use to print.')
+            command=self.get_command(label=_('Enter print command'),
+                                     sublabel=label)
         lpr = os.popen(command,'w')
         de.show_message(
             label=_('Printing via %s')%command,
@@ -23,6 +24,15 @@ class RecRenderer:
         for r in recs:
             exporter.exporter_mult(rd, r, out=lpr,mult=mult,change_units=change_units)
         lpr.close()
+
+    def get_command (self, label="",sublabel="",default_value=None):
+        cmd = de.getEntry(label=label,
+                          sublabel=sublabel,
+                          entryLabel=_('Command: '),
+                          default_value=default_value)
+        if not cmd: raise "User cancelled!"
+        else: return cmd
+    
 
 class SimpleWriter:
     def __init__ (self, file=None, dialog_parent=None, show_dialog=True):
