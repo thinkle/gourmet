c8128cb82bc4d49e496fa0abaabd8838b69303a9
thomas_hinkle <thomas_hinkle>
2005-04-16 03:26:34 +0000

    Merged changes from 0-8-3-4-b

--- src/lib/check_encodings.py
+++ src/lib/check_encodings.py
@@ -87,14 +87,31 @@ class EncodingDialog (de.optionDialog):
                   expander_label=_("See _file with encoding"),
                   encodings=[],
                  ):
+        self.diff_lines = {}
+        self.cursor_already_set = False
         self.expander_label=expander_label
         self.encodings = encodings
+        self.current_error = 0
+        self.diff_texts()
         options = self.create_options()
         expander=self.create_expander()
+        self.setup_buffers()
         de.optionDialog.__init__(self, default=default,label=label, sublabel=sublabel,
                                  options=options, expander=expander)
         self.optionMenu.connect('activate',self.change_encoding)
         self.change_encoding()
+        self.created = False
+
+    def setup_motion_buttons (self):
+        self.hbb = gtk.HButtonBox()
+        self.fb = gtk.Button('Next Difference')
+        self.pb = gtk.Button('Previous Difference')
+        self.pb.connect('clicked',lambda *args: self.move_to_difference(forward=False))
+        self.fb.connect('clicked',lambda *args: self.move_to_difference(forward=True))
+        self.hbb.add(self.pb)
+        self.hbb.add(self.fb)
+        self.evb.add(self.hbb)
+        self.hbb.show_all()
 
     def get_option (self,widget):
         de.optionDialog.get_option(self,widget)
@@ -104,17 +121,87 @@ class EncodingDialog (de.optionDialog):
         return self.encodings.keys()
 
     def create_expander (self):
+        self.evb = gtk.VBox()
         self.sw = gtk.ScrolledWindow()
         self.tv = gtk.TextView()
         self.tv.set_editable(False)
         self.buffer = self.tv.get_buffer()
         self.sw.add(self.tv)
         self.sw.show_all()
-        return self.expander_label,self.sw
+        self.evb.add(self.sw)
+        #self.setup_motion_buttons() # doesn't work yet
+        self.evb.show_all()
+        return self.expander_label,self.evb
+    
+    def setup_buffers (self):
+        self.encoding_buffers={}
+        for k,t in self.encodings.items():
+            self.encoding_buffers[k]=gtk.TextBuffer()
+            self.highlight_tags = [self.encoding_buffers[k].create_tag(background='yellow')]
+            self.line_highlight_tags = [self.encoding_buffers[k].create_tag(background='green')]
+            self.set_buffer_text(self.encoding_buffers[k],t)
 
     def change_encoding (self):
-        self.buffer.set_text(self.encodings[self.ret])
-        debug('changed text to encoding %s'%self.ret,0)
+        #self.buffer.set_text(self.encodings[self.ret])
+        #self.set_buffer_text(self.encodings[self.ret])
+        if self.cursor_already_set:
+            im=self.buffer.get_insert()
+            ti=self.buffer.get_iter_at_mark(im)
+            offset=ti.get_offset()
+        self.tv.set_buffer(self.encoding_buffers[self.ret])
+        self.buffer = self.encoding_buffers[self.ret]
+        debug('changed text to encoding %s'%self.ret,0)        
+        #print 'fdl:',first_diffline,' fdc:',first_diffchar        
+        
+    def move_to_difference (self, forward=True):        
+        dkeys = self.diff_lines.keys()
+        dkeys.sort()
+        if forward:
+            self.current_error += 1
+        else:
+            self.current_error = self.current_error - 1
+        if self.current_error > len(dkeys): self.current_error = 0
+        if self.current_error < 0: self.current_error = len(dkeys)-1
+        mark=self.buffer.create_mark(
+            None,
+            self.buffer.get_iter_at_line_index(dkeys[self.current_error],0),
+            False,
+            )
+        print 'moving to %s'%dkeys[self.current_error]
+        self.tv.scroll_to_mark(mark,0)
+
+        
+        
+    def set_buffer_text (self, buffer, text):
+        for n,l in enumerate(text.split('\n')):
+            if n in self.diff_lines.keys():
+                start = 0
+                diffs = self.diff_lines[n]
+                for sdiff,ediff in diffs:
+                    buffer.insert_with_tags(buffer.get_end_iter(),l[start:sdiff],*self.line_highlight_tags)
+                    buffer.insert_with_tags(buffer.get_end_iter(),l[sdiff:ediff],*self.highlight_tags)
+                    start = ediff
+                buffer.insert_with_tags(buffer.get_end_iter(),l[start:]+'\n',*self.line_highlight_tags)
+            else:
+                buffer.insert_with_tags(buffer.get_end_iter(),l+'\n')
+
+    def diff_texts (self):
+        """Look at our differently encoded buffers for characters where they differ."""
+        encoded_buffers = self.encodings.values()
+        enc1 = encoded_buffers[0]
+        enc_rest = [e.split('\n') for e in encoded_buffers[1:]]
+        for linenum, l in enumerate(enc1.split('\n')):
+            other_lines = [len(e)>linenum and e[linenum] for e in enc_rest]
+            if False in [l==ol for ol in other_lines]:
+                ranges = []
+                for chnum,ch in enumerate(l):
+                    if False in [len(line)>ch and ch == line[chnum] for line in other_lines]:
+                        if ranges and ranges[-1][1]==chnum:
+                            ranges[-1][1]=chnum+1
+                        else:
+                            ranges.append([chnum,chnum+1])
+                self.diff_lines[linenum]=ranges
+                
         
 def getEncoding (*args,**kwargs):
     d=EncodingDialog(*args,**kwargs)
@@ -126,4 +213,4 @@ if __name__ == '__main__':
     print 'selecting file'
     fn=de.select_file('Select file to decode',filters=[['Plain Text',['text/plain'],'*txt']],)
     print 'fn = ',fn
-    print "Got file ", get_file(fn)
+    print "Got file ", get_file(fn)[0:5]
