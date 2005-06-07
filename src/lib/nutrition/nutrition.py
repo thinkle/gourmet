4884729ff2015b76245dc5a05b245eac942d4729
thomas_hinkle <thomas_hinkle>
2005-06-07 03:24:19 +0000

    Add useful objecty things to hold nutrition info and allow easy addition, multiplication, etc.

--- src/lib/nutrition/nutrition.py
+++ src/lib/nutrition/nutrition.py
@@ -1,4 +1,4 @@
-import re
+import re, string
 import sys
 
 # Our basic module for interaction with our nutritional information DB
@@ -21,8 +21,8 @@ class NutritionData:
         """Create an automatic equivalence for ingredient key 'key' and nutritional DB row ROW
         """
         density=self.get_density(key,row)
-        row = self.get_key(key)
-        #if row: self.row.nbdno=row.nbdno
+        row = self._get_key(key)
+        if row: self.row.nbdno=row.nbdno
         else:
             self.db.naliases.append({'nbdno':row.nbdno,
                                   'ingkey':key})
@@ -33,7 +33,57 @@ class NutritionData:
         self.db.naliases.append({'nbdno':nbdno,
                                  'ingkey':key})
 
-    def get_key (self, key):
+    def get_matches_for_string (self, key, max=50):
+        """Handed a string, get a list of likley USDA database matches.
+
+        We return a list of lists:
+        [[description, nutritional-database-number],...]
+
+        If max is not none, we cut our list off at max items (and hope our
+        sorting algorithm succeeded in picking out the good matches!).
+        """
+        words=re.split("\W",key)
+        words = filter(lambda w: w and not w in ['in','or','and','with'], words)
+        if words:
+            # match any of the words in our key
+            regexp = "("+string.join(words,"|")+")" # match any of our words
+            print 'Filtering nview -- %s items'%len(self.db.nview)
+            nvw = self.db.search(self.db.nview,'desc',regexp)
+            print 'Narrowed to %s items'%len(nvw)
+            # create a list of rows and sort it, putting the most likely match first
+            lst = [[r.desc,r.ndbno] for r in nvw]
+            def sort_func (row1,row2):
+                dsc1=row1[0].lower()
+                dsc2=row2[0].lower()
+                sc1=0
+                sc2=0
+                # we presume recipe keys start with important words and proceed to less
+                # important ones -- so we give each word a descending value.
+                # in i.e. milk, low fat, we get the points: milk (3), low (2), fat (1)
+                score = len(words)
+                for w in words:
+                    w=w.lower()
+                    if dsc1.find(w)>=0: sc1+=score
+                    if dsc2.find(w)>=0: sc2+=score
+                    score = score - 1
+                if sc1<sc2:
+                    return 1
+                elif sc2<sc1:
+                    return -1
+                # otherwise, we assume a longer string is a worse match
+                elif len(dsc1)>len(dsc2): return 1
+                elif len(dsc2)>len(dsc1): return -1
+                else: return 0
+            print 'sorting list'
+            lst.sort(sort_func)
+            if max and len(lst) > max:
+                # we cut down the list if it's too long
+                # (so we hope our sorting algorhythm is doing
+                # a good job!)
+                lst = lst[0:max]
+            return lst
+
+    def _get_key (self, key):
         """Handed an ingredient key, get our nutritional Database equivalent
         if one exists."""
         #print 'key=',key
@@ -47,19 +97,29 @@ class NutritionData:
         We return an object interfacing with our DB whose attributes
         will be nutritional values.
         """
-        aliasrow = self.get_key(key)
+        aliasrow = self._get_key(key)
         if aliasrow:
             nvrows=self.db.nview.select({'ndbno':aliasrow.ndbno})
             if len(nvrows)==1:
-                return nvrows[0]
+                return NutritionInfo(nvrows[0])
             elif len(nvrows)>1:
                 raise "Too many rowd returned for ndbno %s"%aliasrow.ndbno
         # if we don't have a nutritional db row, return None
         return None
 
-    def get_conversion_for_amt (self, amt, unit, key, row):
+    def get_conversion_for_amt (self, amt, unit, key, row=None):
+        """Get a conversion for amount amt of unit 'unit' to USDA standard.
+
+        Multiplying our standard numbers (/100g) will get us the appropriate
+        calories, etc.
+
+        get_conversion_for_amt(amt,unit,key) * 100 will give us the
+        number of grams this AMOUNT converts to.
+        """
+        
         # our default is 100g
         cnv=self.conv.converter('g.',unit)
+        if not row: row=self.get_nut_from_key(key)
         if not cnv:
             cnv = self.conv.converter('g.',unit,
                                       density=self.get_density(key,row)
@@ -109,7 +169,8 @@ class NutritionData:
         return densities,units
             
     def get_densities (self,key=None,row=None):
-        if not row: row = self.get_key(key)
+        """Handed key or nutrow, return dictionary with densities."""
+        if not row: row = self._get_key(key)
         if not row: return None
         if self.conv.density_table.has_key(key):
             return self.conv.density_table[key]
@@ -163,6 +224,97 @@ class NutritionData:
             unit = groups[2]
             extra = groups[4]
             return amt,unit,extra
+                    
+class NutritionInfo:
+    """A multipliable way to reference an object.
+
+    Any attribute of object that can be mutiplied, will be returned
+    multiplied by mult.
+
+    We can also support various mathematical operators
+    n = NutritionInfo(obj, mult=2)
+    n * 2 -> NutritionInfo(obj,mult=4)
+    n2 = NutritionInfo(obj2, mult=3)
+    n2 + n -> NutritionInfoList([n2,n])
+
+    The result is that addition and multiplication 'makes sense' for
+    properties. For example, if we have nutrition info for 1 carrot,
+    we can multiply it or add it to the nutrition info for an
+    eggplant. The resulting object will reflect the appropriate
+    cumulative values.
+
+    Carrot = NutritionInfo(CarrotNutritionRow)
+    Eggplant = NutritionInfo(EggplantNutritionRow)
+
+    Carrot.kcal => 41
+    Eggplant.kcal => 24
+    (Carrot + Eggplant).kcal => 65
+    (Carrot * 3 + Eggplant).kcal => 147
+
+    This will be true for all numeric properties.
+
+    Non numeric properties return a somewhat not-useful string:
+    
+    (Carrot + Eggplant).desc => "CARROTS,RAW, EGGPLANT,RAW"
+    """
+    def __init__ (self,rowref, mult=1):
+        self.__rowref__ = rowref
+        self.__mult__ = mult
+
+    def __getattr__ (self, attr):
+        if attr[0]!='_':
+            ret = getattr(self.__rowref__, attr)
+            if type(ret)==float:
+                return ret * self.__mult__
+            else:
+                return ret
+        else:
+            # somehow this magically gets us standard
+            # attribute handling...
+            raise AttributeError, attr
+
+    def __add__ (self, obj):
+        if isinstance(obj,NutritionInfo):
+            return NutritionInfoList([self,obj])
+        elif isinstance(obj,NutritionInfoList):
+            return NutritionInfoList([self]+obj.__nutinfos__)
+
+    def __mul__ (self, n):
+        return NutritionInfo(self.__rowref__, n)
+        
+class NutritionInfoList (list, NutritionInfo):
+    """A summable list of objects.
+
+    When we ask for numeric attributes of our members, we get the sum.
+    """
+    def __init__ (self,nutinfos, mult=1):
+        self.__nutinfos__ = nutinfos
+        self.__mult__ = 1
+
+    def __getattr__ (self, attr):
+        if attr[0]!='_':
+            alist = [getattr(ni,attr) for ni in self.__nutinfos__]
+            try:
+                if alist and type(alist[0])==float:
+                    if self.__mult__: alist = [n * self.__mult__ for n in alist]
+                    return sum(alist)
+            except:
+                return ", ".join(map(str,alist))
+        else:
+            # somehow this magically gets us standard
+            # attribute handling...
+            raise AttributeError, attr
+
+    def __add__ (self, obj):
+        if isinstance(obj,NutritionInfo):
+            return NutritionInfoList(self.__nutinfos__ + [obj])
+        elif isinstance(obj,NutritionInfoList):
+            return NutritionInfoList(self.__nutinfos__ + obj.__nutinfos__)
+
+    def __sub__ (self, obj):
+        copy = self.__nutinfos__[0:]
+        copy.remove(obj)
+        return NutritionInfoList(copy)
             
 if __name__ == '__main__':
     import sys
@@ -182,7 +334,3 @@ if __name__ == '__main__':
         print 'Information: ',row.desc, nd.get_conversions(row=row)
         #print 'Gramweights: ',nd.get_gramweights(row)
         #print 'Density of ',row.desc,' = ',nd.get_densities(row)
-        
-
-    
-    
