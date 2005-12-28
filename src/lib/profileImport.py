c7df38dd36c30d08f907b913b919a1c67d0bf096
thomas_hinkle <thomas_hinkle>
2005-12-28 02:28:42 +0000

    No change really.

--- src/lib/profileImport.py
+++ src/lib/profileImport.py
@@ -6,14 +6,13 @@ import profile, tempfile, os.path
 #    #def run (self,*args,**kwargs):
 #    #    profile.run('ImportTestCase.run(self,*args,**kwargs)')
 
-
-
 it = ImportTest()
 print 'Setting up DB'
 it.setup_db()
 print 'Profiling gourmet importer'
 profi = os.path.join(tempfile.tempdir,'GOURMET_PROFILE')
 profile.run("""it.run_test({'filename':'/home/tom/Desktop/Junk/recipes.grmt'})""",profi)
+#profile.run("""it.run_test({'filename':'/home/tom/Projects/recipe/Data/mealmaster.mmf'})""",profi)
 import pstats
 p = pstats.Stats(profi)
 p.strip_dirs().sort_stats('cumulative').print_stats()
