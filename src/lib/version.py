cc3e8fa2133ce9224d5b5c5fa815a03bbf623954
thomas_hinkle <thomas_hinkle>
2006-03-28 02:47:56 +0000

    Version++

--- src/lib/version.py
+++ src/lib/version.py
@@ -2,7 +2,7 @@
 from gettext import gettext as _
 appname = _("Gourmet Recipe Manager")
 copyright = _("Copyright (c) 2004,2005, Thomas M. Hinkle. GNU GPL")
-version = "0.10.1"
+version = "0.10.2"
 # repeated in setup.cfg
 description = _("Gourmet Recipe Manager is an application to store, organize and search recipes. Gourmet also makes it easy to create shopping lists from recipes. Gourmet imports recipes from a number of sources, including MealMaster and MasterCook archives and several popular websites. Gourmet can export recipes as text, MealMaster files, HTML web pages, and a custom XML format for exchange with other Gourmet users. Gourmet supports linking images with recipes.")
 authors = ["Thomas M. Hinkle <Thomas_Hinkle@alumni.brown.edu>",
