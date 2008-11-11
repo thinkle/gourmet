481b957fc47e748262ae4e3ac19c3f7bbe1b6de0
thomas_hinkle <thomas_hinkle>
2008-11-11 11:43:15 +0000

    Add missing __init__.py file

---
+++ src/lib/plugins/import_export/gxml_plugin/__init__.py
@@ -0,0 +1,7 @@
+import gxml_exporter_plugin
+import gxml_importer_plugin
+
+plugins = [gxml_exporter_plugin.GourmetExporterPlugin,
+           gxml_importer_plugin.GourmetXML2Plugin,
+           gxml_importer_plugin.GourmetXMLPlugin
+           ]
