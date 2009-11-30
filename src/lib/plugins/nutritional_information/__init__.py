a07779aebaeb3dea3deea4b8f2fb700354aa5ced
Thomas M Hinkle <tom@minime.(none)>
2009-11-29 20:54:29 -0500

    2009-11-29  Thomas M Hinkle  <tom@minime>
    	* src/lib/prefsGui.py (PreferencesGui): Make PreferencesGui
    	pluggable.
    
    	* src/lib/plugins/nutritional_information/__init__.py: ++
    	preferences plugin
    
    	* src/lib/plugins/nutritional_information/export_plugin.py (NutritionBaseExporterPlugin):
    	Add preference that allows user to turn off nutritional export
    	altogether or to turn it off when information is only partial.
    
    	* src/lib/plugin.py (BaseExporterPlugin.add_field): Add parameter
    	write_empty_field (which defaults to False) to determine whether
    	we add custom fields with no content.
    	(DatabasePlugin.remove): Change "deactivate" call to "remove",
    	which was what was intended (deactivate is called when the
    	pluggable is destroyed; remove is called when the plugin is
    	deactivated).
    	(PrefsPlugin): Add base class for adding tabs to preferences
    	dialog.
    
    	* src/lib/plugin_loader.py (MasterLoader.register_pluggable): Fix
    	typo.
    
    	* glade/preferenceDialog.glade: Remove hardcoded email
    	preferences (if we want them back, they belong in the email
    	plugin).

--- src/lib/plugins/nutritional_information/__init__.py
+++ src/lib/plugins/nutritional_information/__init__.py
@@ -1,8 +1,10 @@
 import data_plugin, main_plugin, reccard_plugin, export_plugin, shopping_plugin
+import nutPrefsPlugin
 plugins = [
     data_plugin.NutritionDataPlugin,
     main_plugin.NutritionMainPlugin,
     reccard_plugin.NutritionDisplayPlugin,
     export_plugin.NutritionBaseExporterPlugin,
     shopping_plugin.ShoppingNutritionalInfoPlugin,
+    nutPrefsPlugin.NutritionPrefs,
     ]
