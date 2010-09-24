04611574c93e2ab6a5e9276ff9e0a68001ffaaac
Bernhard Reiter <ockham@raz.or.at>
2010-09-24 23:37:38 +0200

    Migrate unit_converter to gtk.Builder

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ result = setup(
                 'gourmet.legacy_db.db_09',
                 'gourmet.plugins',
                 ] + plugins,
-    package_data = {'gourmet': ['plugins/*.gourmet-plugin','plugins/*/*.gourmet-plugin','plugins/*.glade','plugins/*/*.glade', 'plugins/*/images/*.png','plugins/*/*/images/*.png']},
+    package_data = {'gourmet': ['plugins/*.gourmet-plugin','plugins/*/*.gourmet-plugin','plugins/*.glade','plugins/*/*.glade', 'plugins/unit_converter/*.ui', 'plugins/*/images/*.png','plugins/*/*/images/*.png']},
     package_dir = {'gourmet' : os.path.join('src','lib')},
     scripts = script,
     cmdclass={'install_data' : my_install_data},
