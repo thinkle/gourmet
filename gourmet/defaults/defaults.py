cf048baa5c40e2ede56f9739326dc24598e891e2
Bernhard Reiter <ockham@raz.or.at>
2012-12-17 01:48:42 +0100

    Add plurals handling fix by A. Katz.
    "Just a quick patch to fix a bug in default.py.
    The function for converting a plural to a singular unit had two issues.
    a) WORD_TO_SING_PLUR_PAIR keys are lowercase but the word being handed to the function could be in any case
    b) if the word is not in WORD_TO_SING_PLUR_PAIR it does nothing, this patch tries lang.guess_singulars before giving up."
    See https://sourceforge.net/tracker/?func=detail&aid=3420657&group_id=108118&atid=649654

--- gourmet/defaults/defaults.py
+++ gourmet/defaults/defaults.py
@@ -98,8 +98,14 @@ if hasattr(lang,'PLURALS'):
 
 def get_pluralized_form (word, n):
     from gettext import ngettext
-    if WORD_TO_SING_PLUR_PAIR.has_key(word):
-        forms = list(WORD_TO_SING_PLUR_PAIR[word])
+    lword=word.lower()
+    if WORD_TO_SING_PLUR_PAIR.has_key(lword):
+        forms = list(WORD_TO_SING_PLUR_PAIR[lword])
+        forms += [n]
+        return ngettext(*forms)
+    elif  lang.guess_singulars(lword):
+        forms = lang.guess_singulars(lword)
+        forms +=[lword]
         forms += [n]
         return ngettext(*forms)
     else:
