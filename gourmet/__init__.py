6c0d409be6d511e1bb5cc19d7aba27397193851d
Bernhard Reiter <ockham@raz.or.at>
2013-01-05 14:53:26 +0100

    Remove some cruft from gourmet/__init__.py.

--- gourmet/__init__.py
+++ gourmet/__init__.py
@@ -2,11 +2,6 @@
 # You may use and distribute this software under the terms of the
 # GNU General Public License, version 2 or later
 
-cvs_id = "$Id: __init__.py,v 1.6 2007/11/12 04:28:06 thomas_hinkle Exp $"
-
-APPNAME = "Gourmet"
-VERSION = "0.2.2"
-
 import gglobals
 import defaults.defaults as defaults
 import convert
@@ -15,8 +10,6 @@ import shopping
 import os.path
 import ImageExtras
 import OptionParser
-
-#sys.path.append('/usr/share/gourmet') # for debian
 
 from gourmet.OptionParser import *
 
