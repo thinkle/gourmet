65cc3380a49b0f02fb72bcf1c521267291201c5c
Thomas Hinkle <tmhinkle@gmail.com>
2013-01-12 09:54:05 -0500

    meaningless change

--- gourmet/settings.py
+++ gourmet/settings.py
@@ -2,7 +2,7 @@ import os.path
 
 # The following lines are modified at installation time by setup.py so they
 # point to the actual data files installation paths.
-data_dir = '..'
-icon_base = os.path.join(data_dir, "gourmet", "icons")
+data_dir = '/usr/local/share'
+icon_base = '/usr/local/share/icons/hicolor'
 
 locale_base = os.path.join(data_dir, "locale")
