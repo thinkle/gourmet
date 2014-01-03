9bed812202ec4c3955efff51dfcdfdcc0bdb562c
Bernhard Reiter <ockham@raz.or.at>
2014-01-03 21:12:14 +0100

    Fix stupid mistake.

--- gourmet/plugins/import_export/pdf_plugin/__init__.py
+++ gourmet/plugins/import_export/pdf_plugin/__init__.py
@@ -5,6 +5,6 @@ plugins = [pdf_exporter_plugin.PdfExporterPlugin]
 
 # pypoppler -- the python bindings for our PDF display library,
 # which we require for printing -- doesn't work under Windows.
-if sys.platform == "win32":
+if sys.platform != "win32":
     import print_plugin
     plugins.append(print_plugin.PDFPrintPlugin)
