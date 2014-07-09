d39198f223c4abd206ee7caae053aac12cb66175
Bernhard Reiter <ockham@raz.or.at>
2014-07-09 22:58:38 +0200

    Use Descriptive for Nutrition and UsdaWeight classes.

--- gourmet/plugins/nutritional_information/data_plugin.py
+++ gourmet/plugins/nutritional_information/data_plugin.py
@@ -5,20 +5,28 @@ import gourmet.backends.db
 from gourmet.plugin import DatabasePlugin
 import parser_data
 
+from gourmet.models import meta
+from gourmet.models import PluginInfo
+
 class NutritionDataPlugin (DatabasePlugin):
 
     name = 'nutritondata'
     version = 4
 
     def setup_usda_weights_table (self):
-        self.db.usda_weights_table = Table('usda_weights',gourmet.backends.db.Base.metadata,
-                                        Column('id',Integer(),primary_key=True),
-                                        *[Column(name,gourmet.backends.db.map_type_to_sqlalchemy(typ),**{})
-                                          for lname,name,typ in parser_data.WEIGHT_FIELDS]
-                                        )
         class UsdaWeight (object):
-            pass
-        self.db._setup_object_for_table(self.db.usda_weights_table, UsdaWeight)
+            __tablename__ = 'usda_weights'
+
+            id = Column(Integer,primary_key=True)
+            ndbno = Column(Integer)
+            seq = Column(Float)
+            amount = Column(Float)
+            unit = Column(String(80))
+            gramwt = Column(Float)
+            ndata = Column(Integer)
+            stdev = Column(Float)
+
+        self.db.usda_weights_table = UsdaWeight.__table__
 
     def setup_nutritionconversions_table (self):
         class NutritionConversion(gourmet.backends.db.Base):
@@ -48,22 +56,70 @@ class NutritionDataPlugin (DatabasePlugin):
 
     def create_tables (self, *args):
         #print 'nutritional_information.data_plugin.create_tables()'
-        cols = [Column(name,gourmet.backends.db.map_type_to_sqlalchemy(typ),**(name=='ndbno' and {'primary_key':True} or {}))
-                 for lname,name,typ in parser_data.NUTRITION_FIELDS
-                 ] + [Column('foodgroup',Text(),**{})]
-        #print 'nutrition cols:',cols
-        self.db.nutrition_table = Table('nutrition',gourmet.backends.db.Base.metadata,
-                                     *cols
-                                     )
-        class Nutrition (object):
-            pass
-        self.db._setup_object_for_table(self.db.nutrition_table, Nutrition)
+
+        class Nutrition (meta.Base):
+            __tablename__ = 'nutrition'
+
+            ndbno = Column(Integer, primary_key=True)
+            desc = Column(String(100))
+            water = Column(Float)
+            kcal = Column(Float)
+            protein = Column(Float)
+            lipid = Column(Float)
+            ash = Column(Float)
+            carb = Column(Float)
+            fiber = Column(Float)
+            sugar = Column(Float)
+            calcium = Column(Float)
+            iron = Column(Float)
+            magnesium = Column(Float)
+            phosphorus = Column(Float)
+            potassium = Column(Float)
+            sodium = Column(Float)
+            zinc = Column(Float)
+            copper = Column(Float)
+            manganese = Column(Float)
+            selenium = Column(Float)
+            vitaminc = Column(Float)
+            thiamin = Column(Float)
+            riboflavin = Column(Float)
+            niacin = Column(Float)
+            pantoacid = Column(Float)
+            vitaminb6 = Column(Float)
+            folatetotal = Column(Float)
+            folateacid = Column(Float)
+            foodfolate = Column(Float)
+            folatedfe = Column(Float)
+            choline = Column(Float)
+            vitb12 = Column(Float)
+            vitaiu = Column(Float)
+            vitarae = Column(Float)
+            retinol = Column(Float)
+            alphac = Column(Float)
+            betac = Column(Float)
+            betacrypt = Column(Float)
+            lypocene = Column(Float)
+            lutzea = Column(Float)
+            vite = Column(Float)
+            vitk = Column(Float)
+            fasat = Column(Float)
+            famono = Column(Float)
+            fapoly = Column(Float)
+            cholestrl = Column(Float)
+            gramwt1 = Column(Float)
+            gramdsc1 = Column(String(100))
+            gramwt2 = Column(Float)
+            gramdsc2 = Column(String(100))
+            refusepct = Column(Float)
+            foodgroup = Column(Text)
+
+        self.db.nutrition_table = Nutrition.__table__
         
         self.setup_usda_weights_table()
         self.setup_nutritionconversions_table()
         self.setup_nutritionaliases_table()
 
-        #gourmet.backends.db.Base.metadata.create_all(rd.db)
+        meta.Base.metadata.create_all(meta.engine)
         self.db.do_add_nutrition = self.do_add_nutrition
 
     def update_version (self, stored_plugin_version, current_plugin_version):
