a97af28b79af7cf1181b8bbd14c61eb396eb7ac6
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-02-29 20:17:07 -0500

    Add pyglet-based sound

--- gourmet/sound.py
+++ gourmet/sound.py
@@ -1,15 +1,18 @@
 try:
-    from sound_gst import Player
+    from sound_pyglet import Player
 except ImportError:
-    print 'No gst player'
     try:
-        from sound_windows import Player
+        from sound_gst import Player
     except ImportError:
-        print 'No windows player'
-        import sys
-        class Player:
-            """Fallback player"""
-            def play_file (self,path):
-                print 'No player installed -- beeping instead'
-                for n in range(5): sys.stdout.write('\a'); sys.stdout.flush()
+        print 'No gst player'
+        try:
+            from sound_windows import Player
+        except ImportError:
+            print 'No windows player'
+            import sys
+            class Player:
+                """Fallback player"""
+                def play_file (self,path):
+                    print 'No player installed -- beeping instead'
+                    for n in range(5): sys.stdout.write('\a'); sys.stdout.flush()
 
