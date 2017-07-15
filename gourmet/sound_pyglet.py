6bd9f5ddfdfe7e4d6669f2bd0b8cf22340e4283d
Joe Sapp <sappj@ieee.org>
2017-07-14 21:01:25 -0400

    Simplify pyglet player

--- gourmet/sound_pyglet.py
+++ gourmet/sound_pyglet.py
@@ -5,11 +5,8 @@ class Player:
         pass
 
     def play_file (self,path):
-        self.player = pyglet.media.Player()
         self.source = pyglet.media.load(path,streaming=False)
-        self.player.queue(self.source)
-        while not self.player._audio_finished:
-            self.player.play()
+        self.source.play()
 
     def stop_play (self,path):
         pass
