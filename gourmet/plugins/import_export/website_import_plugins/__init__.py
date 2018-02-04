afeb18834770f007227b42c1fa1647f946e7f3d5
Thomas Hinkle <thinkle@Thomass-MBP.home>
2018-02-04 09:05:38 -0500

    new plugins for cooks illustrated and nytimes

--- gourmet/plugins/import_export/website_import_plugins/__init__.py
+++ gourmet/plugins/import_export/website_import_plugins/__init__.py
@@ -3,9 +3,14 @@ import foodnetwork_plugin
 import allrecipes_plugin
 import ica_se_plugin
 import epicurious_plugin
+import nytimes_plugin
+import cooksillustrated_plugin
 plugins = [about_dot_com_plugin.AboutDotComPlugin,
            foodnetwork_plugin.FoodNetworkPlugin,
            allrecipes_plugin.AllRecipesPlugin,
            ica_se_plugin.IcaSePlugin,
            epicurious_plugin.EpicuriousPlugin,
+           nytimes_plugin.NYTPlugin,
+           cooksillustrated_plugin.WebImporterPlugin,
+           cooksillustrated_plugin.CooksIllustratedPlugin,
            ]
