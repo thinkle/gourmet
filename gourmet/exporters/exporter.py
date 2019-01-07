thinkle/gourmet

commit 438c9c8a0a6bb23d894c354dd6cbea195d20f8c2

unicode fix

gourmet/exporters/exporter.py
@@ -199,14 +199,14 @@ class exporter (SuspendableThread, Pluggable):
                 # this 'if' ought to be unnecessary, but is kept around
                 # for db converting purposes -- e.g. so we can properly
                 # export an old DB
-                if ret and type(ret)!=str: 
+                if ret and type(ret) not in [str,unicode]: 
                     ret = convert.seconds_to_timestring(ret,fractions=self.fractions)
-            elif attr=='rating' and ret and type(ret)!=str:
+            elif attr=='rating' and ret and type(ret) not in [str,unicode]:
                 if ret/2==ret/2.0:
                     ret = "%s/5 %s"%(ret/2,_('stars'))
                 else:
                     ret = "%s/5 %s"%(ret/2.0,_('stars'))
-            elif attr=='servings' and type(ret)!=str:
+            elif attr=='servings' and type(ret) not in [str,unicode]:
                 ret = convert.float_to_frac(ret,fractions=self.fractions)
             elif attr=='yields':
                 ret = convert.float_to_frac(ret,fractions=self.fractions)
@@ -513,9 +513,9 @@ class ExporterMultirec (SuspendableThread, Pluggable):
                 # this 'if' ought to be unnecessary, but is kept around
                 # for db converting purposes -- e.g. so we can properly
                 # export an old DB
-                if ret and type(ret)!=str: 
+                if ret and type(ret) not in [str,unicode]: 
                     ret = convert.seconds_to_timestring(ret,fractions=self.fractions)
-            elif attr=='rating' and ret and type(ret)!=str:
+            elif attr=='rating' and ret and type(ret) not in [str,unicode]:
                 if ret/2==ret/2.0:
                     ret = "%s/5 %s"%(ret/2,_('stars'))
                 else:
@@ -551,7 +551,7 @@ class ExporterMultirec (SuspendableThread, Pluggable):
                     os.makedirs(self.outdir)
             else: os.makedirs(self.outdir)
         
-        oneFileOpenByMyself = self.one_file and type(self.out)==str and self.open_files   
+        oneFileOpenByMyself = self.one_file and type(self.out) not in [str,unicode] and self.open_files   
         multiFileOpenByMyself = not self.one_file and self.open_files
                     
         if oneFileOpenByMyself:

commit 82146d21fd7fbda4d520ca502d3e562259c6d82c

Fix: broken 'Export all recipes' (#929)
- Fix for issue #969 - Can't 'Export all recipes'.
- This feature appears to be broken by commits 438c9c8 and 37494d1 with the deletion of variables 'multiFileOpenByMyself' in class ExporterMultirec in module gourmet/exporters/exporter.py. Commits a3cacb0 and 76b8214 introduced variables 'oneFileOpenByMyself' and 'multiFileOpenByMyself' and parameter 'open_files'.
- Changes made:
-- Instantiated 'self.outdir = out' in class ExportMultirec constructor method (__init__)
-- Reintroduced a variable to indicate if a directory for multi files needs to be created, i.e. 'create_multi_file'
-- Renamed 'oneFileOpenByMyself' to 'create_one_file' to better describe what the variable represents
-- Renamed class ExportMultirec parameter 'open_files' to 'create_file' to better describe what the parameter does
- This fix affects class ExporterMultirec and it's parameters.

gourmet/exporters/exporter.py
@@ -464,30 +464,33 @@ class ExporterMultirec (SuspendableThread, Pluggable):
 
     name = 'Exporter'
 
-    def __init__ (self, rd, recipes, out, one_file=True, open_files = True,
-                  ext='txt',
-                  conv=None,
-                  imgcount=1,
-                  exporter=exporter,
-                  exporter_kwargs={},
-                  padding=None):
+    def __init__(self, rd, recipes, out, one_file=True, create_file=True,
+                 ext='txt',
+                 conv=None,
+                 imgcount=1,
+                 exporter=exporter,
+                 exporter_kwargs={},
+                 padding=None):
         """Output all recipes in recipes into a document or multiple
         documents. if one_file, then everything is in one
         file. Otherwise, we treat 'out' as a directory and put
         individual recipe files within it.
 
-        @param one_file If True works in one_file mode. For this the class will
+        @param out: The name of the output file or directory
+
+        @param one_file: If True works in one_file mode. For this the class will
                     create the file which is accessible by self.ofi
                     If this is set to False a directory will be created and the
                     name of the directory will be passed via self.outdir and self.ofi
 
-        @param open_files If this parameter is True the files will be created
+        @param create_file: If this parameter is True the files will be created
                     otherwise to create the file is up to the user.
         """
         self.timer=TimeAction('exporterMultirec.__init__()')
         self.rd = rd
         self.recipes = recipes
         self.out = out
+        self.outdir = out
         self.padding=padding
         self.one_file = one_file
         Pluggable.__init__(self,[BaseExporterMultiRecPlugin])
@@ -499,7 +502,7 @@ class ExporterMultirec (SuspendableThread, Pluggable):
                                                   convert.FRACTIONS_ASCII)
         self.DEFAULT_ENCODING = self.exporter.DEFAULT_ENCODING
         self.one_file = one_file
-        self.open_files = open_files
+        self.create_file = create_file
 
     def _grab_attr_ (self, obj, attr):
         if attr=='category':
@@ -550,8 +553,9 @@ class ExporterMultirec (SuspendableThread, Pluggable):
                     self.outdir=self.unique_name(self.outdir)
                     os.makedirs(self.outdir)
             else: os.makedirs(self.outdir)
-        oneFileOpenByMyself = self.one_file and type(self.out) not in [str,unicode] and self.open_files
-        if oneFileOpenByMyself:
+        create_one_file = self.one_file and type(self.out) in [str, unicode] and self.create_file
+        create_multi_file = not self.one_file and type(self.out) in [str, unicode]
+        if create_one_file:
             self.ofi=open(self.out,'wb')
         else: self.ofi = self.out
         self.write_header()
@@ -564,7 +568,7 @@ class ExporterMultirec (SuspendableThread, Pluggable):
             msg = _("Exported %(number)s of %(total)s recipes")%{'number':self.rcount,'total':self.rlen}
             self.emit('progress',float(self.rcount)/float(self.rlen), msg)
             fn=None
-            if multiFileOpenByMyself:
+            if create_multi_file:
                 fn=self.generate_filename(r,self.ext,add_id=True)
                 self.ofi=open(fn,'wb')
             if self.padding and not first:
@@ -573,12 +577,12 @@ class ExporterMultirec (SuspendableThread, Pluggable):
             self.connect_subthread(e)
             e.do_run()
             self.recipe_hook(r,fn,e)
-            if multiFileOpenByMyself:
+            if create_multi_file:
                 self.ofi.close()
             self.rcount += 1
             first = False
         self.write_footer()
-        if oneFileOpenByMyself:
+        if create_one_file:
             self.ofi.close()
         self.timer.end()
         self.emit('progress',1,_("Export complete."))

gourmet/plugins/import_export/epub_plugin/epub_exporter.py
@@ -287,7 +287,7 @@ class website_exporter (ExporterMultirec):
             self.exportargs['conv']=conv
         ExporterMultirec.__init__(self, rd, recipe_table, out,
                                   one_file=True,
-                                  open_files=False,
+                                  create_file=False,
                                   ext=self.ext,
                                   exporter=epub_exporter,
                                   exporter_kwargs=self.exportargs)