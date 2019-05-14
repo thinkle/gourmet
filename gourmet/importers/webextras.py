d3ffacf44ee3fd416f55d347cb9ce98b842d1f20
Bastian Kleineidam <bastian.kleineidam@web.de>
2019-05-14 11:55:52 +0200

    Move getdatafromurl() to webextras, and cleanup the chefkoch code

--- gourmet/importers/webextras.py
+++ gourmet/importers/webextras.py
@@ -1,4 +1,5 @@
-import socket, gourmet.threadManager, urllib
+import socket, gourmet.threadManager, urllib2
+from gourmet.gdebug import warn
 from gettext import gettext as _
 DEFAULT_SOCKET_TIMEOUT=45.0
 URLOPEN_SOCKET_TIMEOUT=15.0
@@ -20,7 +21,7 @@ class URLReader (gourmet.threadManager.SuspendableThread):
     def read (self):
         message = _('Retrieving %s'%self.url)
         socket.setdefaulttimeout(URLOPEN_SOCKET_TIMEOUT)
-        sock = urllib.urlopen(self.url)
+        sock = urllib2.urlopen(self.url)
         socket.setdefaulttimeout(DEFAULT_SOCKET_TIMEOUT)
         bs = 1024 * 8 # bite size...
         # Get file size so we can update progress correctly...
@@ -78,10 +79,36 @@ def get_url (url, suspendableThread):
     """Return data from URL, possibly displaying progress."""
     if type(url) in [str,unicode]:
         socket.setdefaulttimeout(URLOPEN_SOCKET_TIMEOUT)
-        sock = urllib.urlopen(url)
+        sock = urllib2.urlopen(url)
         socket.setdefaulttimeout(DEFAULT_SOCKET_TIMEOUT)
         return read_socket_w_progress(sock,suspendableThread,_('Retrieving %s'%url))
     else:
         sock = url
         return read_socket_w_progress(sock,suspendableThread,_('Retrieving file'))
+
+
+def getdatafromurl(url, content_type_check=None):
+    """Download data from URL.
+    @param url: the URL to download
+    @ptype url: string
+    @param content_type_check: if non-empty, only return data if the
+      Content-Type header starts with the given string
+    @ptype content_type_check: string
+    @return: URL data or None, and the url (which may be redirected
+      to a new URL)
+    @rtype: tuple (string, string) or (None, string)
+    """
+    data = None
+    try:
+        sock = urllib2.urlopen(url)
+        url = sock.geturl()
+        if content_type_check:
+            content_type = sock.info().get('content-type', 'application/octet-stream')
+            if content_type.lower().startswith(content_type_check):
+                data = sock.read()
+        else:
+            data = sock.read()
+    except urllib2.URLError as msg:
+        warn("could not get data from URL %r: %s" % (url, msg))
+    return data, url
 
