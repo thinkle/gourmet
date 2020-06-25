e30389622eb367513f20b5a566290ae1039cb0b7
saxon-s <saxon-s@theshuttleworths.org>
2020-06-24 21:42:48 -0700

    Chg: update commit based on review comments

--- gourmet/tests/test_plugin_loader.py
+++ gourmet/tests/test_plugin_loader.py
@@ -9,22 +9,28 @@ from gourmet import gglobals
 class Test (unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        # preserve the current gourmet working directory
+        cls.original_gourmetdir = gglobals.gourmetdir
+
         # create a temporary test directory
-        cls.tmp_dir = tempfile.mkdtemp(".pl")
+        cls.tmp_dir = tempfile.mkdtemp()
         gglobals.gourmetdir = cls.tmp_dir
 
-        # continue to import with 'gourmetdir' set to 'tmp_dir', otherwise 'gourmetdir' is set to '~/.gourmet'
+        # Continue to import with 'gourmetdir' set to 'tmp_dir',
+        # Tests need to setup their own test workspace, otherwise 'gourmetdir' is set to '~/.gourmet' which could
+        # result in the user gourmet database and other files being corrupted.
+        # This attempt at isolation only really works if you're running this test module alone, not after others.
         from gourmet.plugin_loader import get_master_loader  # noqa: E402 import not at top of file
 
         cls.ml = get_master_loader()
 
     @classmethod
     def tearDownClass(cls):
+        # restore the original gourmet working directory location
+        gglobals.gourmetdir = cls.original_gourmetdir
+
         # delete temporary test directory
-        try:
-            shutil.rmtree(cls.tmp_dir)
-        except OSError as e:
-            print("Error: {} : {}").format(cls.tmp_dir, e.strerror)
+        shutil.rmtree(cls.tmp_dir)
 
     def testDefaultPlugins (self):
         self.ml.load_active_plugins()
@@ -38,7 +44,7 @@ class Test (unittest.TestCase):
             if module_name not in self.ml.active_plugin_sets:
                 self.ml.activate_plugin_set(plugin_set)
         self.ml.save_active_plugins()
-        self.assertTrue(os.path.exists(self.ml.active_plugin_filename))
+        assert os.path.exists(self.ml.active_plugin_filename)
         self.assertEqual(len(self.ml.errors), 0)  # there should be 0 plugin errors
 
 if __name__ == '__main__':
